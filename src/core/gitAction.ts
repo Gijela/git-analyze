96e3df001e900cd7d727d6d2dba5ac5040655161
Gijela <linjianhui1@xdf.xn>
2025-01-16 13:44:23 +0800

    refactor: remove dependence

--- src/core/gitAction.ts
+++ src/core/gitAction.ts
@@ -1,28 +1,11 @@
 import { simpleGit, SimpleGit } from 'simple-git';
 import { GitOperationError } from './errors';
-import { env } from '../utils/env';
 
 export class GitAction {
   private git: SimpleGit;
 
   constructor() {
-    const config: string[] = [];
-
-    console.log("HTTP_PROXY:", env.HTTP_PROXY);
-    console.log("HTTPS_PROXY:", env.HTTPS_PROXY);
-
-    // 如果存在vpn代理，则使用环境变量
-    if (env.HTTP_PROXY) {
-      config.push(`http.proxy=${env.HTTP_PROXY}`);
-    }
-    if (env.HTTPS_PROXY) {
-      config.push(`https.proxy=${env.HTTPS_PROXY}`);
-    }
-
-    this.git = simpleGit({
-      baseDir: process.cwd(),
-      ...(config.length > 0 ? { config } : {})
-    });
+    this.git = simpleGit({ baseDir: process.cwd() });
   }
 
   async clone(url: string, path: string): Promise<void> {
